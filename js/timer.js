5070895465fe3ac1134c9f0b8ee6bc4d7ef47775
whou <wilshou@gmail.com>
2019-12-19 13:49:02 +1100

    added basic timer functionality

---
+++ js/timer.js
@@ -0,0 +1,51 @@
+class Timer {
+    constructor (timerCounter, timerStartButton) {
+        // from DOM accepts the Counter <h1> and the start button <button>
+        this.timerCounter = timerCounter;
+        this.timerStartButton = timerStartButton;
+
+        // store time left in seconds to this.timeLeft
+        this.timeLeft = this.toSeconds(this.timerCounter.innerText);
+
+        this.timerStartButton.addEventListener('click', this.start);
+    }
+    start = () => {
+        // calls tick until value of timeLeft is 0
+        this.intervalID = setInterval(this.tick, 1000);
+
+        this.finish();
+    };
+    tick = () => {
+        // counts down timer by one tick
+        this.timeLeft--;
+        console.log(this.timeLeft);
+        this.timerCounter.innerText = this.toDisplay(this.timeLeft);
+        if (this.timeLeft <= 0) clearInterval(this.intervalID);
+    };
+    finish = () => {
+        // TODO: refactor into promise format??? Used when finished
+        console.log('FINISHED!!!');
+    };
+    toSeconds = (text) => {
+        // toSeconds will translate display text to seconds
+        return text.split(':').map((x) => parseInt(x)).reduce((seconds, curNum, idx) => {
+            seconds += idx === 0 ? 60 * curNum : curNum;
+            return seconds;
+        }, 0);
+    };
+    toDisplay = (seconds) => {
+        // toDisplay will translate seconds to display text
+        let displayMinutes = Math.floor(seconds / 60);
+        let displaySeconds = seconds % 60;
+        // TODO: pad both sides with 0s
+        return `${displayMinutes}:${displaySeconds}`;
+    };
+}
+
+const timerCounter = document.querySelector('.timer__counter');
+const timerStartButton = document.querySelector('.btn--timer');
+
+const timer = new Timer(timerCounter, timerStartButton);
+
+console.log(timer.toSeconds('4:29'));
+console.log(timer.toDisplay(2));
